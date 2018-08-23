8c9d3296ed515cc2f1f65bd52f4eaef974601c0e
CoreyJNash <CoreyJNash@gmail.com>
2018-08-23 13:53:19 -0400

    New styles when a user does CRUD actions

--- assets/scripts/jokes/jokesui.js
+++ assets/scripts/jokes/jokesui.js
@@ -1,6 +1,6 @@
 'use strict'
 const store = require ('../store.js')
-const jokeTemplate = require ('../alljokes.handlebars')
+const jokeTemplate = require ('../templates/alljokes.handlebars')
 const jokesapi = require ('./jokesapi.js')
 
 
@@ -10,12 +10,12 @@ const onCreateSuccess = function (data) {
   $('#message').css('font-size', '50px')
   $('#message').css('text-shadow', '-1px -1px 0 #000', '1px -1px 0 #000', '-1px 1px 0 #000', '1px 1px 0 #000')
   $('#message').css('text-align', 'center')
-//   $('#message').removeClass('hidden')
+  $('#message').removeClass('hidden')
   $('#create-joke-modal').modal('hide')
   $('#create-joke')[0].reset()
 
   store.joke = data.joke
-  console.log('onCreateSuccess ran. Data is :', data)
+//   console.log('onCreateSuccess ran. Data is :', data)
 }
 
 const onCreateFailure = function (error) {
@@ -24,11 +24,11 @@ const onCreateFailure = function (error) {
     $('#message').css('font-size', '50px')
     $('#message').css('text-shadow', '-1px -1px 0 #000', '1px -1px 0 #000', '-1px 1px 0 #000', '1px 1px 0 #000')
     $('#create-game')[0].reset()
-    console.error('onCreateFailure ran. Error is :', error)
+    // console.error('onCreateFailure ran. Error is :', error)
   }
 
   const showJokesSucess = function (data) {
-      console.log(data)
+    //   console.log(data)
       let htmltemplate = jokeTemplate({jokes: data.jokes})
       $('#show-jokes-list').html(htmltemplate)
       $('.delete').on('click', onDeleteJoke)
@@ -41,17 +41,28 @@ const onCreateFailure = function (error) {
     $('#message').css('font-size', '50px')
     $('#message').css('text-shadow', '-1px -1px 0 #000', '1px -1px 0 #000', '-1px 1px 0 #000', '1px 1px 0 #000')
     $('#message').css('text-align', 'center')
-    // $('#message').removeClass('hidden')
-    $('#show-game-modal').modal('hide')
+    $('#message').removeClass('hidden')
+    $('#show-joke-modal').modal('hide')
    
+  }
+
+  const onDeleteFailure = function () {
+    $('#message').text('This isn\t your joke!  Stop that!')
+    $('#message').css('font-family', 'Shadows Into Light')
+    $('#message').css('font-size', '50px')
+    $('#message').css('text-shadow', '-1px -1px 0 #000', '1px -1px 0 #000', '-1px 1px 0 #000', '1px 1px 0 #000')
+    $('#message').css('text-align', 'center')
+    $('#message').removeClass('hidden') 
+    $('#show-joke-modal').modal('hide')
   }
 
   const onDeleteJoke = (event) => {
     event.preventDefault()
     const jokeId = $(event.target).closest('ul').attr('data-id')
-    console.log("delete: " + jokeId)
+    // console.log("delete: " + jokeId)
     jokesapi.deleteJoke(jokeId)
       .then(onDeleteJokeSucess)
+      .catch(onDeleteFailure)
   }
   
   const showJokesFailure = function (error) {
@@ -59,13 +70,20 @@ const onCreateFailure = function (error) {
     $('#message').css('font-family', 'Shadows Into Light')
     $('#message').css('font-size', '50px')
     $('#message').css('text-shadow', '-1px -1px 0 #000', '1px -1px 0 #000', '-1px 1px 0 #000', '1px 1px 0 #000')
-    console.error('onShowFailure ran. Error is :', error)
+    // console.error('onShowFailure ran. Error is :', error)
   }
 
   
 
   const failure = (error) => {
-    console.error(error)
+    $('#message').text('This isn\t your joke!')
+    $('#message').css('font-family', 'Shadows Into Light')
+    $('#message').css('font-size', '50px')
+    $('#message').css('text-shadow', '-1px -1px 0 #000', '1px -1px 0 #000', '-1px 1px 0 #000', '1px 1px 0 #000')
+    $('#update-modal').modal('hide')
+    $('#message').removeClass('hidden')
+    $('#update-joke')[0].reset()
+    // console.error(error)
   }
 
   const updateJokeSuccess = function (data) {
@@ -74,11 +92,11 @@ const onCreateFailure = function (error) {
     $('#message').css('font-size', '50px')
     $('#message').css('text-shadow', '-1px -1px 0 #000', '1px -1px 0 #000', '-1px 1px 0 #000', '1px 1px 0 #000')
     $('#message').css('text-align', 'center')
-    // $('#message').removeClass('hidden')
+    $('#message').removeClass('hidden')
     $('#update-modal').modal('hide')
-    $('#update-game')[0].reset()
+    $('#update-joke')[0].reset()
     store.jokess = data.jokess
-    console.log('onUpdateSuccess ran. Data is :', data)
+    // console.log('onUpdateSuccess ran. Data is :', data)
    }
 
 module.exports = {
@@ -86,6 +104,7 @@ module.exports = {
     onCreateFailure,
     showJokesSucess,
     onDeleteJoke,
+    onDeleteFailure,
     showJokesFailure,
     failure,
     updateJokeSuccess,
