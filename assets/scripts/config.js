d8105816386b5590325432281ea98748a385f9d9
CoreyJNash <CoreyJNash@gmail.com>
2018-08-23 14:32:42 -0400

    added heroku app link

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://dashboard.heroku.com/apps/glacial-shelf-46176',
   development: 'http://localhost:4741'
 }
 
